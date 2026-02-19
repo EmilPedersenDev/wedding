4be20c8fd50327a7f7024e6bc0720942827a86cc
RefinedWiki Build Agent <bamboo@refinedwiki.com>
2026-02-19 22:54:26 +0100

    adding ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  runtimeConfig: {
+    public: {
+      rsvpFormUrl: "", // e.g. Google Form URL; leave empty to use mailto
+      wedding: {
+        names: "Emil & Anna",
+        date: "Date TBA",
+        time: "Time TBA",
+        venue: "Venue TBA",
+        tagline: "We're getting married!",
+      },
+    },
+  },
+});
